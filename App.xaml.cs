1fa14f55c5754d114b2478dc305cf1ab6951af5a
vyanda kartika <uyandakartikafajarina@gmail.com>
2026-07-01 21:49:49 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using FloodWarnApp.Views;
+using FloodWarnApp.Services; // Pastikan folder Services terbaca
+
+namespace FloodWarnApp
+{
+    public partial class App : Application
+    {
+        // INI DIA YANG HILANG! Kita daftarkan database-nya agar bisa diakses dari semua halaman
+        public static DatabaseService Database { get; private set; } = new DatabaseService();
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new LoginPage());
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using FloodWarnApp.Services;
+using Microsoft.Extensions.Logging;
+
+namespace FloodWarnApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddSingleton<DatabaseService>();
+
+            return builder.Build();
+        }
+    }
+}
